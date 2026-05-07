343917da493c65c33f4ae633fd75db2811cbc298
Bruno Mantuano <mantuano_bruno@id.uff.br>
2026-05-07 01:18:35 -0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,27 @@
+const fs = require("fs");
+
+const caminhoArquivo = process.argv;
+const link = caminhoArquivo[2];
+
+fs.readFile(link, "utf-8", (erro, texto) => {
+  verificaPalavrasDuplicadas(texto);
+});
+
+// criar um array com as palavras
+// contar as ocorrências
+// montar um objeto com o resultado
+//
+//{
+//    "web" = 5,
+//    "computador" = 4
+//}
+
+function verificaPalavrasDuplicadas(texto) {
+  const listaPalavras = texto.split(" ");
+  const resultado = {};
+  //objeto[propriedade] = valor;
+  listaPalavras.forEach((palavra) => {
+    resultado[palavra] = (resultado[palavra] || 0) + 1;
+  });
+  console.log(resultado);
+}
