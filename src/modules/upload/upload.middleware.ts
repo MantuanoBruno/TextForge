f802fabef6eef54353251bf5ae471a0549c52fd0
Bruno Mantuano <mantuano_bruno@id.uff.br>
2026-05-08 18:26:58 -0300

    Add a file filter

--- src/modules/upload/upload.middleware.ts
+++ src/modules/upload/upload.middleware.ts
@@ -1,6 +1,20 @@
 import multer from "multer";
 import path from "path";
 
+const allowedMimeTypes = [
+  "text/plain",
+
+  "application/pdf",
+
+  "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+
+  "image/png",
+  "image/jpeg",
+  "image/webp",
+  "image/tiff",
+  "image/bmp",
+];
+
 const storage = multer.diskStorage({
   destination(req, file, cb) {
     cb(null, "uploads/");
@@ -14,4 +28,16 @@ const storage = multer.diskStorage({
 
 export const upload = multer({
   storage,
+
+  limits: {
+    fileSize: 10 * 1024 * 1024,
+  },
+
+  fileFilter(req, file, cb) {
+    if (!allowedMimeTypes.includes(file.mimetype)) {
+      return cb(new Error("Unsurppoted file type"));
+    }
+
+    cb(null, true);
+  },
 });
