959d04986e9b8330c0963d3097bf455d05f6f884
Bruno Mantuano <mantuano_bruno@id.uff.br>
2026-05-08 19:13:11 -0300

    Add a clean up temporary-files

--- src/modules/ocr/ocr.service.ts
+++ src/modules/ocr/ocr.service.ts
@@ -1,27 +1,31 @@
 import Tesseract from "tesseract.js";
 import path from "path";
 import { preprocessImage } from "./image-preprocessor";
+import { deleteFile } from "../../shared/utils/delete-file";
 
 export async function extractTextFromImage(imagePath: string) {
   const processedImagePath = path.join(
     "uploads",
     `processed-${Date.now()}.png`
   );
+  try {
+    await preprocessImage(imagePath, processedImagePath);
 
-  await preprocessImage(imagePath, processedImagePath);
+    const result = await Tesseract.recognize(
+      imagePath,
 
-  const result = await Tesseract.recognize(
-    imagePath,
+      //idioma
 
-    //idioma
+      "por+eng",
 
-    "por+eng",
-
-    {
-      logger: (info) => {
-        console.log(info);
-      },
-    }
-  );
-  return result.data.text;
+      {
+        logger: (info) => {
+          console.log(info);
+        },
+      }
+    );
+    return result.data.text;
+  } finally {
+    await deleteFile(processedImagePath);
+  }
 }
